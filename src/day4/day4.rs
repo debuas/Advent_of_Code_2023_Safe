c70a741009c807548d43c2ffc36bd3052f97cb6e
Christoph Müller <debuas1999@gmail.com>
2023-12-04 05:52:08 +0100

    setup day 4

---
+++ src/day4/day4.rs
@@ -0,0 +1,28 @@
+
+
+pub fn run_day_4_part_1(){
+    let  input = include_str!("./input.txt");
+    let res = "";
+    println!("Result: {:?}", res)
+}pub fn run_day_4_part_2(){
+    let  input = include_str!("./input.txt");
+    let res = "";
+    println!("Result: {:?}", res)
+}
+
+fn from_input_part1(input :&str){
+
+}
+
+mod tests {
+
+    #[test]
+    fn test_part_1(){
+        let input = !include_str!("./testInput1.txt");
+    }
+
+    #[test]
+    fn test_part_2(){
+        let input = !include_str!("./testInput1.txt");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use crate::day1::part2::run_day1_part_2;
 use crate::day2::part1::run_day_2_part1;
 use crate::day2::part2::run_day_2_part2;
 use crate::day3::day3::{run_day_3_part_1, run_day_3_part_2};
+use crate::day4::day4::{run_day_4_part_1, run_day_4_part_2};
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     tracing_subscriber::fmt::init();
@@ -19,4 +21,6 @@ fn main() {
     run_day_2_part2();
     run_day_3_part_1();
     run_day_3_part_2();
+    run_day_4_part_1();
+    run_day_4_part_2();
 }
