5fcf8c8035c3dcd139c7229060c2ea12d1c317ab
Christoph Müller <debuas1999@gmail.com>
2023-12-08 06:53:54 +0100

    current Stage WIP

--- src/day8/day8.rs
+++ src/day8/day8.rs
@@ -2,25 +2,80 @@ use std::any::Any;
 
 use std::cmp::{Ordering};
 use std::collections::HashMap;
-
-
-
-
+use std::rc::Rc;
 use itertools::{Itertools};
+use itertools::FoldWhile::Continue;
 
 use tracing::{debug, info};
 
-use crate::day7::day7::Combination::{FiveOfAKind, FourOfAKind, FullHouse, HighCard, OnePair, ThreeOfAKind, TwoPairs};
+enum Command {
+    Left, Right
+}
+
+impl TryFrom<char> for Command{
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'L' | 'l' => {Ok( Self::Left)},
+            'R' | 'r' => {Ok( Self::Right)},
+            _ => { Err(()) }
+        }
+    }
+}
+#[derive(Default,Clone,Debug)]
+struct Node {
+    pub key : String,
+    kl : String,
+    kr : String,
+    left : Option<Rc<Self>>,
+    right : Option<Rc<Self>>
+}
+
+impl Node {
+
+    pub fn from_line(line : &str) -> Self {
+        let kv = line.split('=')
+            .collect_tuple::<(&str,&str)>().expect("Should always be size 2")
+
+        ;
+        let lr = kv.1
+            .replace('(',"").replace(')',"")
+            .split(',').collect_tuple::<(&str,&str)>().expect("Left right should always be 2")
+        ;
+
+        let mut node = Node::default();
+        node.key = kv.0.into_string();
+        node.kl = lr.0.into_string();
+        node.kr = lr.1.into_string();
+        node
+    }
+    pub fn traverse_unit_from_pattern(&self ,pattern : &[Command] ,k : &str){
+        let mut current_node = Some(self);
+        let mut counter = 0;
+        while current_node.key != k  {
+            let com = pattern[counter%pattern.len()];
+            current_node = current_node.unwrap().get_Node_from_Command()
+
+        }
+        ;
+    }
+
+    pub fn get_Node_from_Command(&self, command: Command) -> Option<Rc<Node>> {
+        match command {
+            Command::Left => {self.left.clone()}
+            Command::Right => {self.right.clone()}
+        }
+    }
+
+}
+
 
 
 
 
 pub fn run_day_8_part_1() {
     let  input = include_str!("./input.txt");
-    let rounds = from_input_part_1(input);
-
-    let res = rounds.iter().enumerate().into_iter().fold(0 ,|acc,(n,round)| acc+ (n+1) as u64 * round.bid);
-
     info!("Result :  {:?}" , res);
     println!("Result :  {:?}" , res)
 }
@@ -38,6 +93,29 @@ pub fn run_day_8_part_2() {
 
 
 pub fn from_input_part_1(input : &str ) -> Vec<""> {
+    let mut lines =
+    input
+        .lines()
+        .collect_vec();
+    let commands = lines.pop();
+    lines.pop();
+    let mut nodes = lines.iter()
+        .map(|&l| Rc::new(Node::from_line(l)))
+        .collect_vec();
+    let starting_node = &nodes[0].key;
+    let mut nodes_map = HashMap::from_iter(nodes.iter().map(|n|(n.key.clone(), n)));
+    nodes_map.iter_mut().for_each(|(k,n)| {
+        let l = nodes_map.get(&n.kl);
+        let r = nodes_map.get(&n.kr);
+        if let Some(&l) = l {
+            n.left = Some(l.clone());
+        }
+        if let Some(&r) = r {
+            n.right = Some(r.clone());
+        }
+    });
+
+
 
 
     todo!("PART 1")
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
 fn main() {
     tracing_subscriber::fmt::init();
