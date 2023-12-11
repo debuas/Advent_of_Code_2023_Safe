e7eb8cd2231c36ca064c418c532fb920cc2aadaa
Christoph Müller <debuas1999@gmail.com>
2023-12-11 09:01:34 +0100

    adjusted the value

--- src/day11/day11.rs
+++ src/day11/day11.rs
@@ -18,7 +18,7 @@ use crate::day11::day11::Observation::{Galaxy, Space};
 
 pub fn run_day_11_part_1() {
     let  input = include_str!("./input.txt");
-    let res = from_input_part_1(input,1);
+    let res = from_input_part_1(input,2);
     let sum = res.iter().map(|e|{*e.1}).collect_vec().iter().sum::<u64>();
     info!("Result Part 1 :  {:?}" , sum);
     println!("Result Part 1:  {:?}" , sum)
@@ -26,7 +26,7 @@ pub fn run_day_11_part_1() {
 
 pub fn run_day_11_part_2() {
     let  input = include_str!("./input.txt");
-    let res = from_input_part_1(input,1000000-1);
+    let res = from_input_part_1(input,1000000);
     let sum = res.iter().map(|e|{*e.1}).collect_vec().iter().sum::<u64>();
     info!("Result Part 2 :  {:?}" , sum);
     println!("Result Part 2:  {:?}" , sum)
@@ -76,22 +76,22 @@ pub fn from_input_part_1(input : &str, factor : i64) -> HashMap<(I64Vec2, I64Vec
     let expansion_voids_x = void_x.difference(&galaxy_x).collect_vec();
     let expansion_voids_y = void_y.difference(&galaxy_y).collect_vec();
 
-    info!("{} expansions in X count twice",&expansion_voids_x.clone().len());
-    info!("{:?} expansions in X count twice",expansion_voids_x);
-    info!("{} expansions in y count twice",expansion_voids_y.clone().len());
-    info!("{:?} expansions in y count twice",expansion_voids_y);
+    info!("{} expansions in X count {}x",&expansion_voids_x.clone().len(),factor);
+    info!("{:?} expansions in X count {}x",expansion_voids_x,factor);
+    info!("{} expansions in y count {}x",expansion_voids_y.clone().len(),factor);
+    info!("{:?} expansions in y count {}x",expansion_voids_y,factor);
     //Expand all galaxies
     let mut galaxys = galaxy_vec.iter().flat_map(|e| { if let Galaxy(v) = e { Some(v) } else { None } }).copied().collect_vec();
     //Transformed Galaxie for empty space
-    info!("Galaxies Before : {:?}",galaxys);
+    debug!("Galaxies Before : {:?}",galaxys);
     galaxys.iter_mut()
         .for_each(|o|{
             let x_amount = &expansion_voids_x.iter().fold(0i64,|acc,e|if e <= &&o.x {acc +1} else { acc });
             let y_amount = &expansion_voids_y.iter().fold(0i64,|acc,e|if e <= &&o.y {acc +1} else { acc });
-            o.x += if x_amount == &0 {0} else {x_amount * factor} ;
-            o.y += if y_amount == &0 {0} else {y_amount * factor} ;
+            o.x += if x_amount == &0 {0} else {(x_amount * (factor-1))} ;
+            o.y += if y_amount == &0 {0} else {(y_amount * (factor-1))} ;
         });
-    info!("Galaxies After : {:?}",galaxys);
+    debug!("Galaxies After : {:?}",galaxys);
     let distances  = galaxys.iter()
         .combinations(2)
         .map(|a| {
@@ -146,7 +146,7 @@ mod tests {
     #[test]
     fn test_day_11_part_1(){
         let  input = include_str!("./testInput1.txt");
-        let res = from_input_part_1(input,1);
+        let res = from_input_part_1(input,2);
         let sum = res.iter().map(|e|{*e.1}).collect_vec().iter().sum::<u64>();
         //info!("Sum {:?}",sum)
         assert_eq!(sum,374)
@@ -158,8 +158,8 @@ mod tests {
     #[test]
     fn test_day_11_part_2(){
         let  input = include_str!("./testInput1.txt");
-        let res = from_input_part_1(input,9);
-        let res2 = from_input_part_1(input,99);
+        let res = from_input_part_1(input,10);
+        let res2 = from_input_part_1(input,100);
         let sum = res.iter().map(|e|{*e.1}).collect_vec().iter().sum::<u64>();
         let sum2 = res2.iter().map(|e|{*e.1}).collect_vec().iter().sum::<u64>();
         //info!("Sum {:?}",sum)
